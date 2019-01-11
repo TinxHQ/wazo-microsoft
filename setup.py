61e7edbd2798f3a1f0651cb6bb20beb07e0e8081
Genevieve Gilbert <ggilbert@wazo.io>
2019-01-11 14:10:04 -0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='wazo_microsoft',
+    version='1.0',
+    description='Wazo Microsoft authentication connector',
+
+    author='Wazo Authors',
+    author_email='dev@wazo.io',
+
+    url='http://wazo.io',
+
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'wazo_microsoft': ['*/api.yml'],
+    },
+    entry_points={
+        'wazo_auth.external_auth': [
+            'microsoft = wazo_microsoft.auth.plugin:Plugin',
+        ]
+    }
+)
