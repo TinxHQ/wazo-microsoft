b0b5d33a7ce4b8ed3a10a4060fec4e5fe4d33fa4
Genevieve Gilbert <ggilbert@wazo.io>
2019-04-08 15:30:01 -0400

    auth: add config from db

--- wazo_microsoft/auth/http.py
+++ wazo_microsoft/auth/http.py
@@ -11,6 +11,7 @@ from flask import request
 from requests_oauthlib import OAuth2Session
 from wazo_auth import http
 from wazo_auth.exceptions import UserParamException
+from wazo_auth.flask_helpers import Tenant
 
 from .helpers import get_timestamp_expiration
 from .schemas import MicrosoftSchema
@@ -29,26 +30,18 @@ class MicrosoftAuth(http.AuthResource):
 
     def __init__(self, external_auth_service, user_service, config):
         self.authorization_base_url = config[self.auth_type]['authorization_base_url']
-        self.client_id = config[self.auth_type]['client_id']
-        self.client_secret = config[self.auth_type]['client_secret']
         self.external_auth_service = external_auth_service
         self.redirect_uri = config[self.auth_type]['redirect_uri']
         self.scope = config[self.auth_type]['scope']
         self.token_url = config[self.auth_type]['token_url']
         self.user_service = user_service
-        self.oauth2 = OAuth2Session(self.client_id, scope=self.scope, redirect_uri=self.redirect_uri)
-        self.websocket = WebSocketOAuth2(
-            host=config[self.auth_type]['websocket_host'],
-            auth=self.oauth2,
-            external_auth=self.external_auth_service,
-            client_secret=self.client_secret,
-            token_url=self.token_url,
-            auth_type=self.auth_type
-        )
+        self.websocket_host = config[self.auth_type]['websocket_host']
 
     @http.required_acl('auth.users.{user_uuid}.external.microsoft.create')
     def post(self, user_uuid):
+        client_id, client_secret = self._get_external_config()
         self.user_service.get_user(user_uuid)
+        self.oauth2 = OAuth2Session(client_id, scope=self.scope, redirect_uri=self.redirect_uri)
         args, errors = MicrosoftSchema().load(request.get_json())
 
         if errors:
@@ -62,6 +55,14 @@ class MicrosoftAuth(http.AuthResource):
         authorization_url, state = self.oauth2.authorization_url(self.authorization_base_url)
         logger.debug('Authorization url : {}'.format(authorization_url))
 
+        self.websocket = WebSocketOAuth2(
+            host=self.websocket_host,
+            auth=self.oauth2,
+            external_auth=self.external_auth_service,
+            client_secret=client_secret,
+            token_url=self.token_url,
+            auth_type=self.auth_type
+        )
         websocket_thread = Thread(target=self.websocket.run, args=(state, user_uuid), name='websocket_thread')
         websocket_thread.daemon = True
         websocket_thread.start()
@@ -90,8 +91,9 @@ class MicrosoftAuth(http.AuthResource):
         return time.mktime(datetime.now().timetuple()) + 30 > token_expiration
 
     def _refresh_token(self, user_uuid, data):
+        client_id, client_secret = self._get_external_config()
         oauth2 = OAuth2Session(self.client_id, token=data)
-        token_data = oauth2.refresh_token(self.token_url, client_id=self.client_id, client_secret=self.client_secret)
+        token_data = oauth2.refresh_token(self.token_url, client_id=client_id, client_secret=client_secret)
 
         logger.critical('refresh token info: %s', token_data)
         data['refresh_token'] = token_data['refresh_token']
@@ -102,6 +104,12 @@ class MicrosoftAuth(http.AuthResource):
         self.external_auth_service.update(user_uuid, self.auth_type, data)
 
         return self._create_get_response(data)
+
+    def _get_external_config(self):
+        tenant = Tenant.autodetect()
+        config = self.external_auth_service.get_config(self.auth_type, tenant.uuid)
+
+        return config.get('client_id'), config.get('client_secret')
 
     @staticmethod
     def _create_get_response(data):
